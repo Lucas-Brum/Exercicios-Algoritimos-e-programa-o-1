627472f0b063d097c8f123698dbb178f033d1820
lucas-brum <lucasmatheusrodriguesbrum@gmail.com>
2020-03-08 21:15:57 -0300

    Parei com unidade 4 modulo 1 completo

---
+++ Unidade 4/CadastroAluno/src/cadastroaluno/cadastroaluno.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadastroaluno;
+
+/**
+ *
+ * @author Lucas Brum
+ */
+public class cadastroaluno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       FormCadastro formCadastro = new FormCadastro();
+       formCadastro.setSize(300, 200);
+       formCadastro.setVisible(true);
+    }
+    
+}
